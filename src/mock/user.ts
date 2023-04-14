86c756d20a6097fd9ebe11f7aa0e1c14cf589e61
sunhaochen_wb <sunhaochen_wb@qiyi.com>
2023-04-14 18:09:10 +0800

    注释mock改用server

--- src/mock/user.ts
+++ src/mock/user.ts
@@ -1,13 +1,13 @@
 // mock/user.js
 import Mock from 'mockjs'
 
-const user = {
-  name: 'John',
-  age: 18,
-  gender: 'male',
-}
+// const user = {
+//   name: 'John',
+//   age: 18,
+//   gender: 'male',
+// }
 
-Mock.mock('/api/user', 'get', user)
+// Mock.mock('/api/user', 'get', user)
 
 Mock.mock('/api/user', 'post', (options: { body: string }) => {
   const { username, password } = JSON.parse(options.body)
@@ -19,7 +19,7 @@ Mock.mock('/api/user', 'post', (options: { body: string }) => {
       success: true,
       token: 'admin_token',
       username,
-      password,
-    },
+      password
+    }
   }
 })
