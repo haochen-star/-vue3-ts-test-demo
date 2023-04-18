6ef7dc79a6d144983eadab36ab4a37e5d576b9d7
sunhaochen_wb <sunhaochen_wb@qiyi.com>
2023-04-18 14:20:26 +0800

    配置文件路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -21,6 +22,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
